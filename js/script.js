58123b172be78956256a406c054cc88210d7841a
Piotr Doliasz <piotrdoliasz@gmail.com>
2021-05-10 23:35:42 +0200

    toggle task done

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@
         },
     ];
 
-    const addNewTask = () => {
+    const addNewTask = (newTaskContent) => {
         tasks.push({
             content: newTaskContent,
         });
@@ -19,8 +19,13 @@
         render();
     };
 
-    const removeTask = (index) => {
-        tasks.splice(index, 1);
+    const removeTask = (taskIndex) => {
+        tasks.splice(taskIndex, 1);
+        render();
+    };
+
+    const toggleTaskDone = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
         render();
     };
 
@@ -29,7 +34,10 @@
 
         for (const task of tasks) {
             htmslString += `
-                <li>
+                <li 
+                ${task.done ? "style=\"text-decoration: line-through\"" : ""}
+                >
+                <button class="js-done">zrobione</button>
                 <button class="js-remove">usun</button>
                     ${task.content}
                 </li>
@@ -43,6 +51,14 @@
         removeButtons.forEach((removeButton, index) => {
             removeButton.addEventListener("click", () => {
                 removeTask(index);
+            });
+        });
+
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButton, index) => {
+            toggleDoneButton.addEventListener("click", () => {
+                toggleTaskDone(index);
             });
         });
     };
