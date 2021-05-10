c67a4314b8bc334e4f2dd449e0618a2878d268d4
Piotr Doliasz <piotrdoliasz@gmail.com>
2021-05-10 23:10:08 +0200

    adding new tasks

--- js/script.js
+++ js/script.js
@@ -25,8 +25,32 @@
         document.querySelector(".js-tasks").innerHTML = htmslString;
     };
 
+    const addNewTask = () => {
+        tasks.push({
+            content: newTaskContent,
+        });
+
+        render();
+    };
+
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+
+        const newTaskContent = document.querySelector(".js-newTask").value.trim();
+        
+        if(newTaskContent === "") {
+            return;
+        }
+
+        addNewTask(newTaskContent);
+    };
+
     const init = () => {
         render();
+
+        const form = document.querySelector(".js-form");
+
+        form.addEventListener("submit", onFormSubmit);
     };
 
     init();
